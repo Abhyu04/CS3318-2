e03ade15576691f67a6e4d6e4aa39a6fbb45c375
Abhyu04 <132271968+Abhyu04@users.noreply.github.com>
2024-12-01 21:34:02 +0000

    Delete ColourTable4.java

--- ColourTable4.java
+++
@@ -1,7 +0,0 @@
-public void remove(int red, int green, int blue) {
-    String hexString = String.format("%02X%02X%02X", red, green, blue);
-    if (!palette.contains(hexString)) {
-        throw new IllegalArgumentException("Color not in palette");
-    }
-    palette.remove(hexString);
-}
